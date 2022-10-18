7348bd4b07a494ff05b4b8384f0901759730f878
Zofia Stanley <zstanley@Orion-login-3.HPC.MsState.Edu>
2022-10-17 23:17:46 -0500

    going back to serial...

--- obs_loc_for_scda/save_global_loc_err.py
+++ obs_loc_for_scda/save_global_loc_err.py
@@ -2,8 +2,8 @@ import itertools
 import time
 import warnings
 import sys
-from dask import delayed
-from dask.distributed import Client
+#from dask import delayed
+#from dask.distributed import Client
 import xarray as xr
 import numpy as np
 from scipy import linalg
@@ -23,15 +23,15 @@ my_data_dir = proj_dir +'/my_data/20151206.030000'
 
   
 #@delayed
-def compute_loc(lat_lon, ds):
+def compute_loc(ds):
     ''' Get optimal localization radii for a given lat/lon 
     returns ErrorComputer objects ec_ast, ec_sst in that order
     '''
-    lat = lat_lon[0]
-    lon = lat_lon[1]
+    #lat = lat_lon[0]
+    #lon = lat_lon[1]
     
     # Pull out column
-    ds = ds.sel(lat=lat, lon=lon)
+    #ds = ds.sel(lat=lat, lon=lon)
     
     # Check for ocean columns by looking at nans or zeros
     ast_sst_corr = ds['cov_atm_ocn'].sel(atm_lev=126, ocn_lev=1).values
@@ -65,60 +65,69 @@ def main():
     arg = int(sys.argv[1])
     
     ## Open averaged covariances
-    ds = xr.open_dataset(my_data_dir+'/temperature_covariances_averaged.nc', chunks={'lat':5, 'lon':36})
+    ds = xr.open_dataset(my_data_dir+'/temperature_covariances_averaged.nc', chunks={'lat':2, 'lon':72})
     #ds.chunk({lat:5, lon:10, atm_lev=-1, ocn_lev=-1, atm_lev_copy:-1, ocn_lev_copy:-1})
 
     
     # Store lat/lon pairs
     lats = ds['lat'].values[arg:arg+2]
-    lons = ds['lon'].values
+    lons = ds['lon'].values[:3]
     lat_lon_list = list(itertools.product(lats,lons))
 
     
     ## Select these lats
     lat_slice = slice(lats[0], lats[-1], 1)
-    ds = ds.sel(lat=slice_obj)
+    lon_slice = slice(lons[0], lons[-1], 1)
+    ds = ds.sel(lat=lat_slice, lon=lon_slice)
     
-    
+    ## Broadcast data  
+  
     ## Initialize results
     results = [ [None] * 2] * (len(lat_lon_list))
 
-    
     ## Stage computations
-    #for ind in range(len(lat_lon_list)):
-    #        results[ind] = compute_loc(lat_lon_list[ind], ds)
+    for ind in range(len(lat_lon_list)):
+        lat = lat_lon_list[ind][0]
+        lon = lat_lon_list[ind][1]
+        # Pull out column
+        ds_sel = ds.sel(lat=lat, lon=lon)
+        results[ind] = compute_loc(ds_sel)
 
             
     ## Execute results with Dask
     #with Client() as c:
+    #    ds = c.scatter(ds, broadcast=True)
     #    results = c.compute(results, sync=True)
 
-    client = Client()
-    #client.scatter(ds)
-    #client.map(compute_loc, lat_lon_list, ds)
+    #client = Client()
+    #print(client, flush=True)
+
+    #ds = client.scatter(ds, broadcast=True)
+    #futures = client.map(compute_loc, lat_lon_list, ds=ds)
     
+    #client.gather(futures)
     
     
     # Use Dask client to parallelize the workload.
     # Create a futures array to store the futures returned by Dask
-    futures = [ [None] * 2] * (len(lat_lon_list))
+    #futures = [ [None] * 2] * (len(lat_lon_list))
     
     # Scatter the dataframe beforehand
-    ds_future = client.scatter(ds)
+    #ds_future = client.scatter(ds, broadcast=True)
     
-    for ind in range(len(lat_lon_list)):
+    #for ind in range(len(lat_lon_list)):
         # Submit tasks to the dask client in parallel
-        future = client.submit(do_operation, df_future, index, date)
+        #future = client.submit(compute_loc, ds_future, lat_lon_list[ind])
         # Store the returned future in futures list
-        futures[ind] = future
+        #futures[ind] = future
         
-        # Gather the results.
-        _ = client.gather(futures)
+    # Gather the results.
+    #_ = client.gather(futures)
         
     ## Initialize empty data arrays for optimal localization radius for (ast, sst) x (atm, ocn)
     #keys = ['error_unloc_atm', 'error_unloc_ocn', 'locrad_gcr_atm', 'locrad_gcr_ocn', 'error_gcr_atm', \
-            'error_gcr_ocn', 'locrad_gcra_atm', 'locrad_gcra_ocn', 'locatten_gcra_atm',\
-            'locatten_gcra_ocn', 'error_gcra_atm', 'error_gcra_ocn', 'error_eorl_atm', 'error_eorl_ocn']
+    #        'error_gcr_ocn', 'locrad_gcra_atm', 'locrad_gcra_ocn', 'locatten_gcra_atm',\
+    #        'locatten_gcra_ocn', 'error_gcra_atm', 'error_gcra_ocn', 'error_eorl_atm', 'error_eorl_ocn']
     
     #ds['error_unloc_atm'] = xr.zeros_like(ds['ocn_z'].min('ocn_lev'))
     #for key in keys[1:]:
