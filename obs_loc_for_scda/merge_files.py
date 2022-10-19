37003678632017841c1e8307901ee17f720b8aa9
Zofia Stanley <zstanley@Orion-login-3.HPC.MsState.Edu>
2022-10-19 17:11:35 -0500

    explicitly load data to dramatically speed up save operation

--- obs_loc_for_scda/merge_files.py
+++ obs_loc_for_scda/merge_files.py
@@ -9,13 +9,22 @@ my_data_dir = proj_dir +'/my_data/20151206.030000'
 
 def merge_by_lat():
     
-    for lat in range(0, 180, 3):
+    for lat in range(132, 140, 3):
         ## Open optimal localization files
         ds = xr.open_mfdataset(my_data_dir+'/opt_loc_'+str(lat)+'_*.nc', autoclose=True, parallel=True)
         
         ## Save files as single dataset
-        ds.to_netcdf(my_data_dir+'/opt_loc_lat_'+str(lat)+'.nc')
+        ds.load().to_netcdf(my_data_dir+'/opt_loc_lat_'+str(lat)+'.nc')
+
+
+def merge_all():
+
+    ds = xr.open_mfdataset(my_data_dir+'/opt_loc_lat_*.nc', autoclose=True, parallel=True)
+
+    ds.load().to_netcdf(my_data_dir+'/opt_loc_global.nc')
 
 
 if __name__ == '__main__':
-    merge_by_lat()
+    #merge_by_lat()
+    merge_all()
+
