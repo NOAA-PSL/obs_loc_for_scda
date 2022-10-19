0406f11b68eb2995ca74badc5d5bea94dc5202ad
Zofia Stanley <zstanley@Orion-login-4.HPC.MsState.Edu>
2022-10-19 15:26:32 -0500

    loop over columns in serial

--- obs_loc_for_scda/save_global_loc_err.py
+++ obs_loc_for_scda/save_global_loc_err.py
@@ -62,26 +62,22 @@ def compute_loc(ds):
 def main():
     
     ## Get input argument
-    arg = int(sys.argv[1])
-    
-    ## Open averaged covariances
-    ds = xr.open_dataset(my_data_dir+'/temperature_covariances_averaged.nc', chunks={'lat':2, 'lon':72})
-    #ds.chunk({lat:5, lon:10, atm_lev=-1, ocn_lev=-1, atm_lev_copy:-1, ocn_lev_copy:-1})
+    arg_lat = int(sys.argv[1])
+    arg_lon = int(sys.argv[2])    
 
+    ## Open averaged covariances
+    ds = xr.open_dataset(my_data_dir+'/temperature_covariances_averaged.nc', chunks={'lat':2, 'lon':36})
     
     # Store lat/lon pairs
-    lats = ds['lat'].values[arg:arg+2]
-    lons = ds['lon'].values[:3]
+    lats = ds['lat'].values[arg_lat:arg_lat+3]
+    lons = ds['lon'].values[arg_lon:arg_lon+18]
     lat_lon_list = list(itertools.product(lats,lons))
-
     
     ## Select these lats
     lat_slice = slice(lats[0], lats[-1], 1)
     lon_slice = slice(lons[0], lons[-1], 1)
     ds = ds.sel(lat=lat_slice, lon=lon_slice)
     
-    ## Broadcast data  
-  
     ## Initialize results
     results = [ [None] * 2] * (len(lat_lon_list))
 
@@ -93,60 +89,34 @@ def main():
         ds_sel = ds.sel(lat=lat, lon=lon)
         results[ind] = compute_loc(ds_sel)
 
-            
-    ## Execute results with Dask
-    #with Client() as c:
-    #    ds = c.scatter(ds, broadcast=True)
-    #    results = c.compute(results, sync=True)
-
-    #client = Client()
-    #print(client, flush=True)
-
-    #ds = client.scatter(ds, broadcast=True)
-    #futures = client.map(compute_loc, lat_lon_list, ds=ds)
-    
-    #client.gather(futures)
-    
-    
-    # Use Dask client to parallelize the workload.
-    # Create a futures array to store the futures returned by Dask
-    #futures = [ [None] * 2] * (len(lat_lon_list))
-    
-    # Scatter the dataframe beforehand
-    #ds_future = client.scatter(ds, broadcast=True)
-    
-    #for ind in range(len(lat_lon_list)):
-        # Submit tasks to the dask client in parallel
-        #future = client.submit(compute_loc, ds_future, lat_lon_list[ind])
-        # Store the returned future in futures list
-        #futures[ind] = future
-        
-    # Gather the results.
-    #_ = client.gather(futures)
-        
     ## Initialize empty data arrays for optimal localization radius for (ast, sst) x (atm, ocn)
-    #keys = ['error_unloc_atm', 'error_unloc_ocn', 'locrad_gcr_atm', 'locrad_gcr_ocn', 'error_gcr_atm', \
-    #        'error_gcr_ocn', 'locrad_gcra_atm', 'locrad_gcra_ocn', 'locatten_gcra_atm',\
-    #        'locatten_gcra_ocn', 'error_gcra_atm', 'error_gcra_ocn', 'error_eorl_atm', 'error_eorl_ocn']
+    keys = ['error_unloc_atm', 'error_unloc_ocn', 'locrad_gcr_atm', 'locrad_gcr_ocn', 'error_gcr_atm', \
+            'error_gcr_ocn', 'locrad_gcra_atm', 'locrad_gcra_ocn', 'locatten_gcra_atm',\
+            'locatten_gcra_ocn', 'error_gcra_atm', 'error_gcra_ocn', 'error_eorl_atm', 'error_eorl_ocn']
     
-    #ds['error_unloc_atm'] = xr.zeros_like(ds['ocn_z'].min('ocn_lev'))
-    #for key in keys[1:]:
-    #    ds[key] = xr.zeros_like(ds['error_unloc_atm'])
+    keys_ast = [key+'_ast' for key in keys]
+    keys_sst = [key+'_sst' for key in keys]   
+    
+    ds['error_unloc_atm_ast'] = xr.zeros_like(ds['ocn_z'].min('ocn_lev'))
+    ds['error_unloc_atm_sst'] = xr.zeros_like(ds['error_unloc_atm_ast'])
+    
+    for key in keys[1:]:
+        ds[key+'_ast'] = xr.zeros_like(ds['error_unloc_atm_ast'])
+        ds[key+'_sst'] = xr.zeros_like(ds['error_unloc_atm_ast'])
 
     
     # Save localization radii
-    #for ind in range(len(lat_lon_list)):
+    for ind in range(len(lat_lon_list)):
         
-    #    lat = lat_lon_list[ind][0]
-    #    lon = lat_lon_list[ind][1]
+        lat = lat_lon_list[ind][0]
+        lon = lat_lon_list[ind][1]
         
-    #    if results[ind][0] not None:
-    #        for key in keys:
-    #            ds[key+'_ast'].loc[dict(lat=lat, lon=lon)] = results[ind][0][key]
-    #            ds[key+'_sst'].loc[dict(lat=lat, lon=lon)] = results[ind][1][key]
+        if results[ind][0] is not None:
+            for key in keys:
+                ds[key+'_ast'].loc[dict(lat=lat, lon=lon)] = results[ind][0].__dict__[key]
+                ds[key+'_sst'].loc[dict(lat=lat, lon=lon)] = results[ind][1].__dict__[key]
 
-        
-    #    ds[keys].to_netcdf(my_data_dir+'/opt_loc_'+str(arg)+'.nc')
+        ds[keys_ast+keys_sst].to_netcdf(my_data_dir+'/opt_loc_'+str(arg_lat)+'_'+str(arg_lon)+'.nc')
     
     
     
